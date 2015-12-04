5bf444d8c67213f83b9bc1559e699eeb8f64aa85
Darren Smith <github@darrenjs.net>
2015-12-04 00:21:52 +0000

    new standalone example

---
+++ examples/hello/hello.cc
@@ -0,0 +1,38 @@
+/* Basic example of use jalson */
+
+#include <jalson/jalson.h>
+
+#include <iostream>
+
+int main(int, char**)
+{
+  // obtain details about the JSON implementation wrapped inside jalson
+
+  jalson::vendor_details details;
+  jalson::get_vendor_details(&details);
+
+  std::cout << "JSON implementation: "
+            << details.vendor << " "
+            << details.major_version << "."
+            << details.minor_version << "."
+            << details.micro_version << "\n";
+
+  // Build a hello JSON object
+
+  jalson::json_value v;
+  jalson::decode(v, "[\"hello\", {}, 2015]");
+
+  // Add some items programmatically
+
+  v.as_array().push_back("world");
+  v.as_array().push_back(1);
+  v.as_array().push_back(true);
+  v.as_array().push_back( jalson::json_object() );
+  v.as_array().push_back( jalson::json_array() );
+  v[1]["vendor"]  = details.vendor;
+  v[1]["version"] = details.major_version;
+
+  // Print
+  std::cout << v << "\n";
+  return 0;
+}
