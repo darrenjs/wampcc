453a842d0d3bfe6799182b200ed93f34ad68aea5
Darren Smith <github@darrenjs.net>
2016-02-23 21:19:50 +0000

    tidyup of automake

--- autotools_setup.sh
+++ autotools_setup.sh
@@ -1,9 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
+cd `dirname "$0"`
 
 echo "===== Autoconf setup script ====="
 
 # placate autotools
 ln -snf README.md README
 ln -snf LICENSE COPYING
+
+test -d || mkdir -p m4
+
 autoreconf -fiv
