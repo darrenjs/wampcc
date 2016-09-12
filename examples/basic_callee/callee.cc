f6a881a00cec040388abce0db775480bc63c5ad1
Darren Smith <git@darrenjs.net>
2016-09-12 07:02:08 +0100

    simplified

--- examples/basic_callee/callee.cc
+++ examples/basic_callee/callee.cc
@@ -17,24 +17,20 @@ void rpc(wamp_invocation& invoke)
 int main(int, char**)
 {
   try {
-    auto __logger = logger::stdlog(std::cout,
-                                   logger::levels_upto(logger::eError), 0);
-
-    std::unique_ptr<kernel> the_kernel( new XXX::kernel({}, __logger));
+    std::unique_ptr<kernel> the_kernel( new XXX::kernel({}, logger::nolog() ));
     the_kernel->start();
 
     std::promise<void> promise_on_close;
-
-    auto fn = [&promise_on_close](XXX::session_handle, bool is_open){
-      if (!is_open)
-        promise_on_close.set_value();
-    };
 
     // Attempt to make a socket connection & build a wamp_session
     auto wconn = wamp_connector::create(
       the_kernel.get(),
       "127.0.0.1", "55555",
-      false, fn);
+      false,
+      [&promise_on_close](XXX::session_handle, bool is_open){
+        if (!is_open)
+          promise_on_close.set_value();
+      } );
 
     auto connect_future = wconn->get_future();
     auto connect_status = connect_future.wait_for(std::chrono::milliseconds(100));
@@ -58,11 +54,9 @@ int main(int, char**)
     // Session is now open, register an RPC
     session->provide("inline", jalson::json_object(), rpc);
 
-    // wait until we get disconnected
+    // Wait until we get disconnected
     promise_on_close.get_future().wait();
 
-    session->close().wait();
-    session.reset();
     return 0;
   }
   catch (std::exception& e)
