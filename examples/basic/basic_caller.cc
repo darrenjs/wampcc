fb74d00c0eaa3266a6790092c845b6184c6977b1
Darren Smith <git@darrenjs.net>
2016-09-22 20:08:16 +0100

    basic caller example

---
+++ examples/basic/basic_caller.cc
@@ -0,0 +1,73 @@
+#include "XXX/kernel.h"
+#include "XXX/rawsocket_protocol.h"
+#include "XXX/wamp_connector.h"
+#include "XXX/wamp_session.h"
+#include "XXX/websocket_protocol.h"
+
+#include <memory>
+#include <iostream>
+
+using namespace XXX;
+
+void rpc(wamp_invocation& invoke)
+{
+  invoke.yield( { jalson::json_array({"hello", "world"}) ,{} } );
+}
+
+int main(int, char**)
+{
+  try
+  {
+    std::unique_ptr<kernel> the_kernel( new XXX::kernel({}, logger::nolog() ));
+
+    auto wconn = wamp_connector::create( the_kernel.get(),
+                                         "127.0.0.1", "55555",
+                                         true );
+
+    auto connect_status = wconn->completion_future().wait_for(std::chrono::milliseconds(100));
+
+    if (connect_status == std::future_status::timeout)
+      throw std::runtime_error("time-out during network connect");
+
+    std::promise<void> ready_to_exit;
+
+    std::shared_ptr<wamp_session> session = wconn->create_session<rawsocket_protocol>(
+      [&ready_to_exit](XXX::session_handle, bool is_open){
+        if (!is_open)
+          try {
+            ready_to_exit.set_value();
+          } catch (...) {}
+      });
+
+    /* Logon to a WAMP realm, and wait for session to be deemed open */
+    client_credentials credentials;
+    credentials.realm="default_realm";
+    credentials.authid="peter";
+    credentials.authmethods = {"wampcra"};
+    credentials.secret_fn = []() -> std::string { return "secret2"; };
+
+    auto session_open_fut = session->initiate_hello(credentials);
+
+    if (session_open_fut.wait_for(std::chrono::milliseconds(5000)) == std::future_status::timeout)
+      throw std::runtime_error("time-out during session logon");
+
+    /* Session is now open, call a remote procedure */
+    wamp_args call_args;
+    call_args.args_list = jalson::json_value::make_array();
+    session->call("greeting", {}, call_args, [&ready_to_exit](wamp_call_result){
+        try {
+        ready_to_exit.set_value();
+        } catch (...) {}
+      });
+
+    ready_to_exit.get_future().wait();
+    std::cout << "going into exit\n";
+    return 0;
+  }
+  catch (std::exception& e)
+  {
+    std::cout << e.what() << std::endl;
+    return 1;
+  }
+}
+
