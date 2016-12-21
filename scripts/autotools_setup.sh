aaff9cdced8cfb755ba8ab9e3af63f6777334e68
Darren Smith <git@darrenjs.net>
2016-12-21 12:00:06 +0000

    support configuring & building jalson as a submodule

--- scripts/autotools_setup.sh
+++ scripts/autotools_setup.sh
@@ -6,5 +6,13 @@ echo "===== Autoconf setup script ====="
 
 
 test -d m4 || mkdir -p m4
+test -e README || ln -snf README.md README
+
+# if we have a bundled jalson project, then setup that up also
+if [ -e jalson/README.md ];
+then
+    (cd jalson && ln -snf README.md README)
+    (cd jalson && mkdir -p m4)
+fi
 
 autoreconf -fiv
