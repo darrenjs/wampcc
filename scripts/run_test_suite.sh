ce0f2616f77fd29fefdc376a7536e9430e824b87
Darren Smith <git@darrenjs.net>
2017-01-01 11:59:34 +0000

    added new test case

--- scripts/run_test_suite.sh
+++ scripts/run_test_suite.sh
@@ -10,10 +10,10 @@ test_early_wamp_session_destructor          23500
 test_late_wamp_session_destructor           23600
 test_late_dealer_destructor                 23700
 test_evthread_wamp_session_destructor       23800
-test_dealer_disconnect_when_has_connections 23900
+#test_dealer_disconnect_when_has_connections 23900
+test_wamp_session_fast_close                24000
 
-
-
+echo Note: test test_dealer_disconnect_when_has_connections has been commented out.
 
 
 
