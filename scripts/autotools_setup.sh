c67bd0e9d7b1ad2d8088132d069d01e38b38e62c
Darren Smith <git@darrenjs.net>
2017-01-14 14:23:33 +0000

    manual check for libtool

--- scripts/autotools_setup.sh
+++ scripts/autotools_setup.sh
@@ -4,6 +4,10 @@ cd  `dirname "$0"`/..
 
 echo "===== Autoconf setup script ====="
 
+# check for libtool
+$ command -v libtool >/dev/null 2>&1 || { \
+    echo "warning! libtool not found ... configure stage might fail" >&2; \
+}
 
 test -d m4 || mkdir -p m4
 test -e README || ln -snf README.md README
