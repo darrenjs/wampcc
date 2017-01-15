1a04617cfff8511c53edc1627749b97983b44a64
Darren Smith <git@darrenjs.net>
2017-01-15 23:12:20 +0000

    typo

--- scripts/autotools_setup.sh
+++ scripts/autotools_setup.sh
@@ -5,7 +5,7 @@ cd  `dirname "$0"`/..
 echo "===== Autoconf setup script ====="
 
 # check for libtool
-$ command -v libtool >/dev/null 2>&1 || { \
+command -v libtool >/dev/null 2>&1 || { \
     echo "warning! libtool not found ... configure stage might fail" >&2; \
 }
 
