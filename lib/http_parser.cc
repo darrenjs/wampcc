7b6e7e767ff97b80aa0213f1ba6990f06d720bd9
Darren Smith <git@darrenjs.net>
2017-03-01 20:19:04 +0000

    fix TODO, and clang-format

--- lib/http_parser.cc
+++ lib/http_parser.cc
@@ -16,34 +16,28 @@
 #include <string.h>
 
 
-
 namespace wampcc
 {
 
+unsigned int http_parser::error() const { return m_parser->http_errno; }
 
-unsigned int http_parser::error() const {
-  return m_parser->http_errno;
-}
+bool http_parser::good() const { return error() == HPE_OK; }
 
-bool http_parser::good() const {
-  return error() == HPE_OK;
-}
+bool http_parser::is_upgrade() const { return m_parser->upgrade != 0; }
 
-bool http_parser::is_upgrade() const {
-  return m_parser->upgrade != 0;
-}
-
-std::string http_parser::error_text() const {
-  return std::string(  ::http_errno_description((enum ::http_errno)m_parser->http_errno) );
+std::string http_parser::error_text() const
+{
+  return std::string(
+      ::http_errno_description((enum ::http_errno)m_parser->http_errno));
 }
 
 
 http_parser::http_parser(parser_type pt)
-  : m_settings( new ::http_parser_settings ),
-    m_parser( new ::http_parser ),
+  : m_settings(new ::http_parser_settings),
+    m_parser(new ::http_parser),
     m_http_status_code(0)
 {
-  ::http_parser_settings_init( m_settings.get() );
+  ::http_parser_settings_init(m_settings.get());
 
   if (pt == e_http_request)
     ::http_parser_init(m_parser.get(), HTTP_REQUEST);
@@ -55,45 +49,51 @@ http_parser::http_parser(parser_type pt)
   // set up the callbacks, using lambdas without captures, so that these lambdas
   // can be assigned to function pointers.
 
-  m_settings->on_headers_complete=[](::http_parser * p) {
-    auto hp =(wampcc::http_parser*)p->data; return hp->on_headers_complete(); };
+  m_settings->on_headers_complete = [](::http_parser* p) {
+    auto hp = (wampcc::http_parser*)p->data;
+    return hp->on_headers_complete();
+  };
 
-  m_settings->on_url=[](::http_parser* p, const char *s, size_t n) {
-    auto hp =(wampcc::http_parser*)p->data; return hp->on_url(s,n); };
+  m_settings->on_url = [](::http_parser* p, const char* s, size_t n) {
+    auto hp = (wampcc::http_parser*)p->data;
+    return hp->on_url(s, n);
+  };
 
-  m_settings->on_header_field=[](::http_parser* p, const char *s, size_t n) {
-    auto hp =(wampcc::http_parser*)p->data; return hp->on_header_field(s,n); };
+  m_settings->on_header_field = [](::http_parser* p, const char* s, size_t n) {
+    auto hp = (wampcc::http_parser*)p->data;
+    return hp->on_header_field(s, n);
+  };
 
-  m_settings->on_header_value=[](::http_parser* p, const char *s, size_t n) {
-    auto hp =(wampcc::http_parser*)p->data; return hp->on_header_value(s,n); };
+  m_settings->on_header_value = [](::http_parser* p, const char* s, size_t n) {
+    auto hp = (wampcc::http_parser*)p->data;
+    return hp->on_header_value(s, n);
+  };
 
-  m_settings->on_status=[](::http_parser* p, const char *s, size_t n) {
-    auto hp =(wampcc::http_parser*)p->data; return hp->on_status(s,n); };
-
+  m_settings->on_status = [](::http_parser* p, const char* s, size_t n) {
+    auto hp = (wampcc::http_parser*)p->data;
+    return hp->on_status(s, n);
+  };
 }
 
 
 http_parser::~http_parser()
 {
+  /* Need destructor here, even empty, so that when the unique_ptr destructor
+   * for http_parser_settings is instantiated it can see the definition of
+   * http_parser_settings. */
 }
-
 
 void http_parser::store_current_header_field()
 {
-  if (!m_current_field.empty())
-  {
-    auto it = m_headers.find( m_current_field );
-    if (it != m_headers.end())
-    {
-      // TODO: test this append does actuall work
-      std::string t = it->second + "," + m_current_value;
-      m_headers.insert({m_current_field, t}  );
+  if (!m_current_field.empty()) {
+    /* It's possible that a HTTP header field might be duplicated, in which case
+     * we combine the values together in a comma separated list. */
+    auto it = m_headers.find(m_current_field);
+    if (it != m_headers.end()) {
       it->second.append(",");
-      it->second.append(m_current_value);
-    }
-    else
-    {
-      m_headers.insert({ m_current_field, m_current_value});
+      it->second.append(std::move(m_current_value));
+    } else {
+      m_headers.insert({m_current_field, m_current_value});
     }
   }
 
@@ -102,22 +102,16 @@ void http_parser::store_current_header_field()
 }
 
 
-int http_parser::on_url(const char *s, size_t n)
-{
-  return 0;
-}
+int http_parser::on_url(const char* s, size_t n) { return 0; }
 
 
-int http_parser::on_header_field(const char *s, size_t n)
+int http_parser::on_header_field(const char* s, size_t n)
 {
-  if (m_state == eParsingField)
-  {
-    m_current_field += {s,n};
-  }
-  else
-  {
+  if (m_state == eParsingField) {
+    m_current_field += {s, n};
+  } else {
     store_current_header_field();
-    m_current_field = {s,n};
+    m_current_field = {s, n};
     m_state = eParsingField;
   }
 
@@ -125,17 +119,14 @@ int http_parser::on_header_field(const char *s, size_t n)
 }
 
 
-int http_parser::on_header_value(const char *s, size_t n)
+int http_parser::on_header_value(const char* s, size_t n)
 {
-  if (m_state == eParsingField)
-  {
-    m_current_value = {s,n};
+  if (m_state == eParsingField) {
+    m_current_value = {s, n};
     m_state = eParsingValue;
-  }
-  else
-  {
-    m_current_value += {s,n};
-  }
+  } else
+    m_current_value += {s, n};
+
 
   return 0;
 }
@@ -144,11 +135,7 @@ int http_parser::on_header_value(const char *s, size_t n)
 size_t http_parser::handle_input(char* const data, size_t const len)
 {
   if (m_state != eComplete)
-  {
-    return ::http_parser_execute(
-      m_parser.get(), m_settings.get(),
-      data, len);
-  }
+    return ::http_parser_execute(m_parser.get(), m_settings.get(), data, len);
   else
     throw std::runtime_error("http parse already complete");
 }
@@ -165,9 +152,8 @@ int http_parser::on_headers_complete()
 
 int http_parser::on_status(const char* s, size_t n)
 {
-  m_http_status += {s,n};
+  m_http_status += {s, n};
   return 0;
 }
-
 
 }
--- lib/wampcc/http_parser.h
+++ lib/wampcc/http_parser.h
@@ -12,7 +12,7 @@
 #include <string>
 #include <memory>
 
-//types brought in from nodejs http-parser project
+// types brought in from nodejs http-parser project
 struct http_parser;
 struct http_parser_settings;
 
@@ -21,18 +21,14 @@ namespace wampcc
 
 class http_parser
 {
- public:
-
+public:
   static constexpr unsigned int status_code_switching_protocols = 101;
 
-  enum parser_type {
-    e_http_request,
-    e_http_response
-  };
+  enum parser_type { e_http_request, e_http_response };
 
   static bool is_http_get(const char* s, size_t n)
   {
-    return n>3 && s[0]=='G' && s[1]=='E' && s[2]=='T' && isspace(s[3]);
+    return n > 3 && s[0] == 'G' && s[1] == 'E' && s[2] == 'T' && isspace(s[3]);
   }
 
   http_parser(parser_type);
@@ -43,7 +39,7 @@ class http_parser
   size_t handle_input(char* const, size_t const);
 
   /** have we completed parsing headers? */
-  bool complete() const { return m_state==eComplete; }
+  bool complete() const { return m_state == eComplete; }
 
   /** does header indicate connection upgrade? */
   bool is_upgrade() const;
@@ -58,7 +54,7 @@ class http_parser
   bool good() const;
 
   /** is field present in headers? */
-  bool has(const char* s) const { return m_headers.find(s) != m_headers.end();}
+  bool has(const char* s) const { return m_headers.find(s) != m_headers.end(); }
 
   /** return header field, otherwise throw */
   const std::string& get(const std::string& field) const
@@ -71,43 +67,34 @@ class http_parser
   }
 
   /* HTTP response status-line textual phrase */
-  const std::string & http_status_phrase() const { return m_http_status; }
+  const std::string& http_status_phrase() const { return m_http_status; }
 
   /* HTTP response status-line code */
   unsigned int http_status_code() const { return m_http_status_code; }
 
-
- private:
-
+private:
   void store_current_header_field();
   int on_headers_complete();
-  int on_url(const char *s, size_t n);
-  int on_header_field(const char *s, size_t n);
-  int on_header_value(const char *s, size_t n);
-  int on_status(const char *s, size_t n);
+  int on_url(const char* s, size_t n);
+  int on_header_field(const char* s, size_t n);
+  int on_header_value(const char* s, size_t n);
+  int on_status(const char* s, size_t n);
 
   std::map<std::string, std::string> m_headers;
 
   std::unique_ptr<::http_parser_settings> m_settings;
   std::unique_ptr<::http_parser> m_parser;
 
-  enum state
-  {
-    eParsingField = 0,
-    eParsingValue,
-    eComplete
-  };
+  enum state { eParsingField = 0, eParsingValue, eComplete };
   state m_state = eParsingField;
 
   std::string m_current_field;
   std::string m_current_value;
 
   unsigned int m_http_status_code;
-  std::string  m_http_status;
+  std::string m_http_status;
 };
-
 }
-
 
 
 #endif
