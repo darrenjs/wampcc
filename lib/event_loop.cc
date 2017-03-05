25636a15136af5aa09cb40a0220fbd2188aea5d6
Darren Smith <git@darrenjs.net>
2017-03-05 09:37:52 +0000

    simplify

--- lib/event_loop.cc
+++ lib/event_loop.cc
@@ -14,42 +14,36 @@
 
 #include <iostream>
 
-namespace wampcc {
+namespace wampcc
+{
 
 struct event
 {
-  enum event_type
-  {
-    null = 0,
-    kill,
-    function_dispatch,
-    timer_dispatch
-  } type;
+  enum event_type { kill = 0, function_dispatch, timer_dispatch } type;
 
-  event(event_type t)
-    : type(t)
-  {}
+  event(event_type t) : type(t) {}
 
-  virtual ~event(){}
+  virtual ~event() {}
 };
 
 
 struct ev_function_dispatch : event
 {
-  ev_function_dispatch(std::function<void()> fn_) :
-    event(event::function_dispatch),
-    fn(std::move(fn_))
-  {}
+  ev_function_dispatch(std::function<void()> fn_)
+    : event(event::function_dispatch), fn(std::move(fn_))
+  {
+  }
 
   std::function<void()> fn;
 };
 
+
 struct ev_timer_dispatch : event
 {
-  ev_timer_dispatch(event_loop::timer_fn fn_) :
-    event(event::timer_dispatch),
-    fn(std::move(fn_))
-  {}
+  ev_timer_dispatch(event_loop::timer_fn fn_)
+    : event(event::timer_dispatch), fn(std::move(fn_))
+  {
+  }
 
   event_loop::timer_fn fn;
 };
@@ -59,23 +53,21 @@ event_loop::event_loop(kernel* k)
   : m_kernel(k),
     __logger(k->get_logger()),
     m_continue(true),
-    m_kill_event( std::make_shared< event > (event::kill) ),
     m_thread(&event_loop::eventmain, this)
 {
 }
 
 
-event_loop::~event_loop()
-{
-  sync_stop();
-}
+event_loop::~event_loop() { sync_stop(); }
 
 
 void event_loop::sync_stop()
 {
+  auto kill_event = std::make_shared<event>(event::kill);
+
   {
     std::lock_guard<std::mutex> guard(m_mutex);
-    m_queue.push_back(m_kill_event);
+    m_queue.push_back(std::move(kill_event));
     m_condvar.notify_one();
   }
 
@@ -90,7 +82,7 @@ void event_loop::dispatch(std::function<void()> fn)
 
   {
     std::lock_guard<std::mutex> guard(m_mutex);
-    m_queue.push_back( std::move(event) );
+    m_queue.push_back(std::move(event));
     m_condvar.notify_one();
   }
 }
@@ -102,14 +94,15 @@ void event_loop::dispatch(std::chrono::milliseconds delay, timer_fn fn)
 }
 
 
-void event_loop::dispatch(std::chrono::milliseconds delay, std::shared_ptr<event> sp)
+void event_loop::dispatch(std::chrono::milliseconds delay,
+                          std::shared_ptr<event> sp)
 {
   auto tp_due = std::chrono::steady_clock::now() + delay;
-  auto event  = std::make_pair(tp_due, std::move(sp));
+  auto event = std::make_pair(tp_due, std::move(sp));
 
   {
     std::lock_guard<std::mutex> guard(m_mutex);
-    m_schedule.insert( std::move(event) );
+    m_schedule.insert(std::move(event));
     m_condvar.notify_one();
   }
 }
@@ -121,85 +114,61 @@ void event_loop::eventloop()
    * they are stored as shared pointers.  This allows other parts of the code to
    * take ownership of the resource, if they so wish.
    */
-  std::list< std::shared_ptr<event> > to_process;
-  while (m_continue)
-  {
+  std::list<std::shared_ptr<event>> to_process;
+  while (m_continue) {
     to_process.clear();
-
     {
       std::unique_lock<std::mutex> guard(m_mutex);
 
-      while (m_continue && m_queue.empty() && m_queue.size()==0)
-      {
+      while (m_continue && m_queue.empty()) {
+
         // identify range of scheduled events which are now due
         const auto tp_now = std::chrono::steady_clock::now();
         const auto upper_iter = m_schedule.upper_bound(tp_now);
 
-        if (upper_iter == m_schedule.begin())
-        {
-          // no events due, so need to sleep
+        if (upper_iter == m_schedule.begin()) {
+          // no events due now so need to sleep, which is either indefinitely or
+          // until the next scheduled item
           if (m_schedule.empty())
-          {
             m_condvar.wait(guard);
-          }
-          else
-          {
+          else {
             auto sleep_for = m_schedule.begin()->first - tp_now;
             m_condvar.wait_for(guard, sleep_for);
           }
-        }
-        else
-        {
+        } else {
           for (auto iter = m_schedule.begin(); iter != upper_iter; ++iter)
-            m_queue.push_back( std::move(iter->second) );
+            m_queue.push_back(std::move(iter->second));
           m_schedule.erase(m_schedule.begin(), upper_iter);
         }
       }
-      to_process.swap( m_queue );
-
-      // m_condvar.wait_for(guard,
-      //                    sleep_interval,
-      //                    [this](){ return !m_queue.empty() && m_queue.size()>0; } );
-      // to_process.swap( m_queue );
-
+      to_process.swap(m_queue);
     }
 
-    for (auto & ev : to_process)
-    {
-      if (ev == m_kill_event)
-      {
-        m_continue = false;
-        return;
-      }
-
-      try
-      {
-        switch ( ev->type )
-        {
-          case event::function_dispatch :
-          {
-            ev_function_dispatch * ev2 = dynamic_cast<ev_function_dispatch*>(ev.get());
+    for (auto& ev : to_process) {
+      try {
+        switch (ev->type) {
+          case event::function_dispatch: {
+            ev_function_dispatch* ev2 =
+                dynamic_cast<ev_function_dispatch*>(ev.get());
             ev2->fn();
             break;
           }
-          case event::timer_dispatch :
-          {
-            ev_timer_dispatch * ev2 = dynamic_cast<ev_timer_dispatch*>(ev.get());
+          case event::timer_dispatch: {
+            ev_timer_dispatch* ev2 = dynamic_cast<ev_timer_dispatch*>(ev.get());
             auto repeat_ms = ev2->fn();
             if (repeat_ms.count() > 0)
               dispatch(repeat_ms, std::move(ev));
             break;
           }
-          default: break;
+          case event::kill: {
+            m_continue = false;
+            return;
+          }
         }
-      }
-      catch ( const std::exception& ex)
-      {
-        LOG_ERROR( "exception during process_event : " << ex.what() );
-      }
-      catch ( ... )
-      {
-        LOG_ERROR( "unknown exception during process_event" );
+      } catch (const std::exception& ex) {
+        LOG_ERROR("exception during process_event : " << ex.what());
+      } catch (...) {
+        LOG_ERROR("unknown exception during process_event");
       }
     } // loop end
   }
@@ -210,13 +179,9 @@ void event_loop::handle_exception(const char* stage)
 {
   try {
     throw;
-  }
-  catch (const std::exception& e)
-  {
+  } catch (const std::exception& e) {
     LOG_WARN("ignoring exception in eventmain, " << stage << ": " << e.what());
-  }
-  catch (...)
-  {
+  } catch (...) {
     LOG_WARN("ignoring exception in eventmain. " << stage << ": unknown type");
   }
 }
@@ -225,27 +190,21 @@ void event_loop::handle_exception(const char* stage)
 /* Entry point for the kernel's EV thread */
 void event_loop::eventmain()
 {
-  scope_guard undo_thread_id([this](){
-      m_ev_thread_id.release();
-    });
+  scope_guard undo_thread_id([this]() { m_thread_id.release(); });
 
-  m_ev_thread_id.set_value(std::this_thread::get_id());
-
+  m_thread_id.set_value(std::this_thread::get_id());
 
   if (m_kernel->get_config().event_loop_start_fn)
     try {
       m_kernel->get_config().event_loop_start_fn();
-    }
-    catch(...) {
+    } catch (...) {
       handle_exception("at start");
     }
 
-  while (m_continue)
-  {
+  while (m_continue) {
     try {
       eventloop();
-    }
-    catch(...) {
+    } catch (...) {
       handle_exception("at main");
     }
   }
@@ -253,8 +212,7 @@ void event_loop::eventmain()
   if (m_kernel->get_config().event_loop_end_fn)
     try {
       m_kernel->get_config().event_loop_end_fn();
-    }
-    catch(...) {
+    } catch (...) {
       handle_exception("at end");
     }
 }
@@ -262,7 +220,7 @@ void event_loop::eventmain()
 
 bool event_loop::this_thread_is_ev() const
 {
-  return m_ev_thread_id.compare(std::this_thread::get_id());
+  return m_thread_id.compare(std::this_thread::get_id());
 }
 
 } // namespace wampcc
--- lib/wampcc/event_loop.h
+++ lib/wampcc/event_loop.h
@@ -18,23 +18,25 @@
 #include <list>
 #include <map>
 
-namespace wampcc {
+namespace wampcc
+{
 
-struct logger;
 class kernel;
 struct event;
+struct logger;
 
 /** Event thread */
 class event_loop
 {
 public:
-
   /* Signature for timer callbacks that can be registered with the event loop.
    * The return value indicates the delay to use for subsequent invocation of
    * the timer function, or 0 if the function should not be invoked again. */
   typedef std::function<std::chrono::milliseconds()> timer_fn;
 
   event_loop(kernel*);
+  event_loop(const event_loop&) = delete;
+  event_loop& operator=(const event_loop&) = delete;
   ~event_loop();
 
   /** Perform synchronous stop of the event loop.  On return, the EV thread will
@@ -51,8 +53,6 @@ public:
   bool this_thread_is_ev() const;
 
 private:
-  event_loop(const event_loop&); // no copy
-  event_loop& operator=(const event_loop&); // no assignment
 
   void handle_exception(const char* stage);
   void eventloop();
@@ -60,18 +60,18 @@ private:
 
   void dispatch(std::chrono::milliseconds, std::shared_ptr<event>);
 
-  kernel * m_kernel;
-  logger & __logger; /* name chosen for log macros */
+  kernel* m_kernel;
+  logger& __logger; /* name chosen for log macros */
 
   bool m_continue;
 
-  std::shared_ptr<event> m_kill_event;
-  std::list< std::shared_ptr<event> > m_queue;
+  std::list<std::shared_ptr<event>> m_queue;
   std::mutex m_mutex;
   std::condition_variable m_condvar;
-  std::multimap< std::chrono::steady_clock::time_point, std::shared_ptr<event>  > m_schedule;
+  std::multimap<std::chrono::steady_clock::time_point, std::shared_ptr<event>>
+      m_schedule;
 
-  synchronized_optional<std::thread::id> m_ev_thread_id;
+  synchronized_optional<std::thread::id> m_thread_id;
 
   std::thread m_thread; // prefer as final member, avoid race conditions
 };
