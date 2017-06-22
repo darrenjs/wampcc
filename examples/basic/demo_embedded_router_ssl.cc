d9ed6d10be5c4cb5b7a2d4590feb2b6855f01d0a
Darren Smith <git@darrenjs.net>
2017-06-22 23:01:38 +0100

    fix minor compile error

--- examples/basic/demo_embedded_router_ssl.cc
+++ examples/basic/demo_embedded_router_ssl.cc
@@ -36,9 +36,7 @@ int main(int argc, char** argv)
     /* Create the listen socket configuration, to support SSL/TLS and all
      * available protocols & message formats. */
     wamp_router::listen_options opts;
-    opts.flags = wamp_router::listen_options::ssl |
-      wamp_router::listen_options::all_sockets |
-      wamp_router::listen_options::all_formats;
+    opts.ssl = true;
     opts.service=argv[1];
     opts.af = tcp_socket::addr_family::inet4;
 
