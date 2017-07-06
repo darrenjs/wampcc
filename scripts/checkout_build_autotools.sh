b2a91706581e124be884658b79c108f70b0b1b38
Darren Smith <git@darrenjs.net>
2017-07-06 06:23:09 +0100

    fix typo

--- scripts/checkout_build_autotools.sh
+++ scripts/checkout_build_autotools.sh
@@ -11,14 +11,13 @@ export LIBUV_HOME=/home/$(whoami)/opt/libuv-1.10.2
 export JANSSON_HOME=/home/$(whoami)/opt/jansson-2.10
 
 export CXX=/usr/bin/clang++-3.5
-export CXXFLAGS="-O0 -g3"
 
 unset LD_LIBRARY_PATH
 
 #----- script body follows -----
 
 # create base dir
-test -d $BASEDIR && rm -rf $$BASEDIR
+test -d $BASEDIR && rm -rf $BASEDIR
 mkdir -p $BASEDIR $SOURCEDIR
 cd $BASEDIR
 
@@ -46,7 +45,7 @@ $SOURCEDIR/wampcc/configure --prefix=${INSTALLDIR} --with-jansson=$JANSSON_HOME 
 make -j || make
 make install
 
-# Now build examples
+# Now build examples against the installed version
 
 export WAMPCC_HOME=$INSTALLDIR
 export LD_LIBRARY_PATH=${WAMPCC_HOME}/lib:${JANSSON_HOME}/lib:${LIBUV_HOME}/lib
